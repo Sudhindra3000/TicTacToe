8fbf5a99c3904c0e860c19abf2ca69f6c57c8360
Sudhindra <sudhindra5678@gmail.com>
2021-05-05 21:32:38 +0530

    show Play Again and End Game Button after game is over

--- app/src/main/java/com/sudhindra/tictactoe/ui/screens/GameUi.kt
+++ app/src/main/java/com/sudhindra/tictactoe/ui/screens/GameUi.kt
@@ -1,9 +1,18 @@
 package com.sudhindra.tictactoe.ui.screens
 
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -16,9 +25,11 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sudhindra.tictactoe.ui.game.GameBoard
 import com.sudhindra.tictactoe.ui.game.PlayersScreen
 import com.sudhindra.tictactoe.ui.utils.isPortrait
+import com.sudhindra.tictactoe.viewmodels.GameState
 import com.sudhindra.tictactoe.viewmodels.GameViewModel
 import com.sudhindra.tictactoe.viewmodels.factories.GameViewModelFactory
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun GameUi(
     player1: String,
@@ -33,6 +44,7 @@ fun GameUi(
     val constraintSet = ConstraintSet {
         val playersScreen = createRefFor("playersScreen")
         val board = createRefFor("board")
+        val bottomButtons = createRefFor("bottomButtons")
 
         constrain(playersScreen) {
             if (portrait) {
@@ -44,6 +56,10 @@ fun GameUi(
             }
         }
         constrain(board) { centerTo(parent) }
+        constrain(bottomButtons) {
+            centerHorizontallyTo(parent)
+            bottom.linkTo(parent.bottom, 20.dp)
+        }
     }
     ConstraintLayout(constraintSet, modifier = Modifier.fillMaxSize()) {
         val responsiveSizeModifier =
@@ -67,5 +83,28 @@ fun GameUi(
             onItemSelect = { viewModel.select(it) },
             enabled = true
         )
+        if (gameState !is GameState.Playing)
+            GameBottomButtons(
+                Modifier.layoutId("bottomButtons"),
+                onPlayAgainClick = {},
+                onEndGameClick = {}
+            )
+    }
+}
+
+@Composable
+fun GameBottomButtons(
+    modifier: Modifier = Modifier,
+    onPlayAgainClick: () -> Unit,
+    onEndGameClick: () -> Unit
+) {
+    Row(modifier) {
+        Button(onClick = onPlayAgainClick, shape = CircleShape, contentPadding = PaddingValues(vertical = 12.dp, horizontal = 20.dp)) {
+            Text(text = "PLAY AGAIN", style = MaterialTheme.typography.h6)
+        }
+        Spacer(Modifier.size(20.dp))
+        Button(onClick = onEndGameClick, shape = CircleShape, contentPadding = PaddingValues(vertical = 12.dp, horizontal = 20.dp)) {
+            Text(text = "END GAME", style = MaterialTheme.typography.h6)
+        }
     }
 }
