f8b23fd8e82e3cafb606a98637add8ac7c45d8f7
Sudhindra <sudhindra5678@gmail.com>
2021-06-24 12:53:49 +0530

    better way to calculate size of square in GameLines

--- app/src/main/java/com/sudhindra/tictactoe/ui/game/GameLines.kt
+++ app/src/main/java/com/sudhindra/tictactoe/ui/game/GameLines.kt
@@ -2,13 +2,11 @@ package com.sudhindra.tictactoe.ui.game
 
 import androidx.compose.animation.core.Animatable
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.StrokeCap
@@ -21,40 +19,41 @@ fun GameLines(
     modifier: Modifier = Modifier,
     gameState: GameState,
 ) {
-    val lineColor = MaterialTheme.colors.secondary
+    BoxWithConstraints(modifier) {
+        val lineColor = MaterialTheme.colors.secondary
 
-    var a by remember { mutableStateOf(0f) }
+        val a = constraints.maxWidth
 
-    val lineOffsets =
-        remember {
-            Animatable(
-                LineOffsets(
-                    Offset(Float.MAX_VALUE, Float.MAX_VALUE),
-                    Offset(Float.MAX_VALUE, Float.MAX_VALUE)
-                ),
-                LineOffsets.VectorConverter
-            )
+        val lineOffsets =
+            remember {
+                Animatable(
+                    LineOffsets(
+                        Offset(Float.MAX_VALUE, Float.MAX_VALUE),
+                        Offset(Float.MAX_VALUE, Float.MAX_VALUE)
+                    ),
+                    LineOffsets.VectorConverter
+                )
+            }
+
+        LaunchedEffect(gameState) {
+            if (gameState is GameState.Won) {
+                val offsets = gameState.winningLine.getWinningLine(a.toFloat())
+                val center = offsets.center
+
+                lineOffsets.snapTo(LineOffsets(center, center))
+                lineOffsets.animateTo(offsets)
+            }
         }
 
-    LaunchedEffect(gameState) {
-        if (gameState is GameState.Won) {
-            val offsets = gameState.winningLine.getWinningLine(a)
-            val center = offsets.center
-
-            lineOffsets.snapTo(LineOffsets(center, center))
-            lineOffsets.animateTo(offsets)
+        Canvas(modifier) {
+            if (gameState is GameState.Won)
+                drawLine(
+                    lineColor,
+                    start = lineOffsets.value.start,
+                    end = lineOffsets.value.end,
+                    strokeWidth = 50f,
+                    cap = StrokeCap.Round
+                )
         }
-    }
-
-    Canvas(modifier) {
-        a = size.width
-        if (gameState is GameState.Won)
-            drawLine(
-                lineColor,
-                start = lineOffsets.value.start,
-                end = lineOffsets.value.end,
-                strokeWidth = 50f,
-                cap = StrokeCap.Round
-            )
     }
 }
